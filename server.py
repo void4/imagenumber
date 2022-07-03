df64ae37e5cee8e07b57686c8f1b9f187e75cca6
void4 <void4@noreply.github.com>
2022-07-03 21:07:32 +0200

    use latestdiff to sort

--- server.py
+++ server.py
@@ -45,11 +45,11 @@ def r_index_get():
 			latest_file = max(userattempts, key=os.path.getctime)
 			latestdiff = compare(latest_file, f"targets/{target['name']}/target.png")["diff"]
 			#score = latestdiff*log(2+len(userattempts)+len(userattempts))
-			# "score": score, 
+			# "score": score,
 			target["scoreboard"].append({"name": os.path.split(userpath)[-1], "latestdiff": latestdiff, "attempts": len(userattempts)})
 
 		print(target)
-		target["scoreboard"] = list(sorted(target["scoreboard"], key=lambda sc: sc["score"]))
+		target["scoreboard"] = list(sorted(target["scoreboard"], key=lambda sc: sc["latestdiff"]))#score
 
 	return jsonify({"status": "ok", "targets": fulltargets})
 
