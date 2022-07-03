13730098518ff48a6fe6887c36ea7798694d0c77
void4 <void4@noreply.github.com>
2022-07-03 21:39:46 +0200

    Fix scoreboard

--- server.py
+++ server.py
@@ -42,13 +42,13 @@ def r_index_get():
 		target["scoreboard"] = []
 		for userpath in glob(f"targets/{target['name']}/attempts/*"):
 			for run in glob(userpath+"/*/"):
-				userattempts = glob(os.path.join(userpath, run, "*.png"))
+				userattempts = glob(os.path.join(run, "*.png"))
 				if len(userattempts):
 					latest_file = max(userattempts, key=os.path.getctime)
 					latestdiff = compare(latest_file, f"targets/{target['name']}/target.png")["diff"]
 					#score = latestdiff*log(2+len(userattempts)+len(userattempts))
 					# "score": score,
-					target["scoreboard"].append({"name": os.path.split(userpath)[-1], "run": run, "latestdiff": latestdiff, "attempts": len(userattempts)})
+					target["scoreboard"].append({"name": os.path.split(userpath)[-1], "run": run.split("/")[-2], "latestdiff": latestdiff, "attempts": len(userattempts)})
 
 		print(target)
 		target["scoreboard"] = list(sorted(target["scoreboard"], key=lambda sc: sc["latestdiff"]))#score
